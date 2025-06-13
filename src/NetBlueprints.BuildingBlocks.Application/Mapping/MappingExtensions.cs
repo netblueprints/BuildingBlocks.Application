ad283ea2e5734baed2058e1e7e46d422fffdac14
Kamyab <valipourkamyab.official@gmail.com>
2025-06-13 10:42:35 +0330

    Add MappingExtensions class for queryable data mapping

---
+++ src/NetBlueprints.BuildingBlocks.Application/Mapping/MappingExtensions.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using NetBlueprints.BuildingBlocks.Application.Models;
+
+namespace NetBlueprints.BuildingBlocks.Application.Mapping;
+public static class MappingExtensions
+{
+    public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize, CancellationToken cancellationToken = default) where TDestination : class
+        => PaginatedList<TDestination>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize, cancellationToken);
+
+    public static Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, IConfigurationProvider configuration, CancellationToken cancellationToken = default) where TDestination : class
+        => queryable.ProjectTo<TDestination>(configuration).AsNoTracking().ToListAsync(cancellationToken);
+}
